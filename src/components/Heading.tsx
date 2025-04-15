520286d5e2d1c2881163928f10611656d664a103
OmarAhmedHabib <omarahmedhabib6@gmail.com>
2025-04-15 14:44:33 +0200

    add the ainmtion motion

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-
+import { motion } from "framer-motion";
 interface HeadingProps {
   title: string;
   isCentered?: boolean;
@@ -7,13 +7,20 @@ interface HeadingProps {
 
 const Heading: FC<HeadingProps> = ({ title, isCentered }) => {
   return (
-    <h1
-      className={`${
-        isCentered && "text-center"
-      } bg-clip-text text-transparent bg-gradient-to-tr from-sky-200 via-sky-500 to-sky-200 font-extrabold text-3xl sm:text-4xl lg:text-5xl`}
-    >
-      {title}
-    </h1>
+   
+
+<motion.h1
+  initial={{ opacity: 0, y: 40 }}
+  whileInView={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.6, ease: "easeOut" }}
+  viewport={{ once: true, amount: 0.6 }} // once: true = تشتغل مرة واحدة فقط
+  className={`${
+    isCentered && "text-center"
+  } bg-clip-text text-transparent bg-gradient-to-tr from-sky-200 via-sky-500 to-sky-200 font-extrabold text-3xl sm:text-4xl lg:text-5xl`}
+>
+  {title}
+</motion.h1>
+
   );
 };
 
